fba5a66534c4258d988332d706d5ef469b0154b6
ArkabaevDima <43188881+ArkabaevDima@users.noreply.github.com>
2018-12-25 00:13:28 +0300

    lab2

---
+++ Tester.java
@@ -0,0 +1,8 @@
+package ru.mirea.arkabaev.lab2;
+
+public class Tester {
+    public static void main(String[] args) {
+        Tringle t1 = new Tringle(10.0,15.0,20.0);
+        System.out.println("Площадь триугольника = " + t1 .getArea());
+    }
+}
