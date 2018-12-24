71220140339f70ce2d67e8d56c354e251557f110
ArkabaevDima <43188881+ArkabaevDima@users.noreply.github.com>
2018-12-25 00:14:02 +0300

    lab4

---
+++ Printable.java
@@ -0,0 +1,8 @@
+package ru.mirea.arkabaev.lab4;
+
+public interface Printable {
+    void println();
+}
+
+
+
---
+++ Tester_Printable.java
@@ -0,0 +1,12 @@
+package ru.mirea.arkabaev.lab4;
+
+public	class	Tester_Printable	{
+    public static	void	main(String[]	args)	{
+        Car c1	=	new Car("Suzuki",	"Vitara",	"GL",	1649000, 2015);
+        c1.println();
+        Car	c2	=	new	Car("BMW",	"1	Series",	"118i",	2900000,	2017);
+        c2.println();
+        Car	c3	=	new	Car("Audi",	"A3",	"Sedan",	1830000,	2016);
+        c3.println();
+    }
+}
